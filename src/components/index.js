aae991ffd3676f7c2e0a8df2638ced4a43256304
ayushtya9i <86874794+ayushtya9i@users.noreply.github.com>
2024-03-02 13:39:16 +0530

    final

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Signup from "../pages/Signup";
+import RTE from "./RTE";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import Select from "./Select";
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  PostForm,
+  Button,
+  PostCard,
+  AuthLayout,
+  Input,
+  Select,
+};
